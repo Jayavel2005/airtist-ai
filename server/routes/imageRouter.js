b78b81becdff0a0d5186001f1f62dcd0fead0922
Jayavel <jayavel1520@gmail.com>
2026-01-28 13:53:38 +0530

    feat: protected route enabled

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import {
-    generateImage,
-    deleteImage,
-    getAllImages,
-    downloadImage
+  generateImage,
+  deleteImage,
+  getAllImages,
+  downloadImage,
 } from "../controllers/image.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
@@ -11,9 +11,7 @@ export const imgRouter = Router();
 
 // imgRouter.use(authMiddleware);
 
-// imgRouter.get("/", getAllImages);
-imgRouter.get("/", authMiddleware, getAllImages);
-
+imgRouter.get("/", getAllImages);
 imgRouter.post("/generate", generateImage);
 imgRouter.get("/download/:id", downloadImage);
 imgRouter.delete("/:id", deleteImage);
